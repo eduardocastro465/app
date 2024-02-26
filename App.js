5fb3e4f6d0367481e9821049a8efe7582223feaa
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-02-26 08:30:15 -0600

    26/02/2024 navegacion

--- App.js
+++ App.js
@@ -1,67 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {  StyleSheet, Text,  View } from 'react-native';
-import Producto from './Componentes/Producto';
-import Productos from './Componentes/Productos';
+import { Boton } from './Componentes/Atomicos'
+import {Calculadora} from './Componentes/Calculadora'
 import Clima from './Componentes/Clima';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Encabezado/>
-      <Cuerpo/>
-      <Pie/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Menu' component={Home}/>
+        <Stack.Screen name='Calculadora' component={Calculadora}/>
+        <Stack.Screen name='Clima' component={Clima}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-export const Encabezado=()=>{
-  return(
-    <View style={styles.encabezado}>
-        <Text style={styles.texto}>X</Text>
-        <Text style={styles.texto}>Encabezados</Text>
-        <Text style={styles.texto}>X</Text>
-    </View>
-  )
-}
 
-export const Cuerpo=()=>{
-  return(
-    <View style={styles.cuerpo}>
-      <Text>Hola</Text>
-      <Clima/>
-    </View>
-  )
-}
+const Stack= createNativeStackNavigator(); 
 
-export const Pie=()=>{
-  //aqui podria tener varias operaciones sin problemas 
+const Home=({navigation})=>{
   return(
-    <View style={styles.pie}>
-        <Text style={styles.texto}>X</Text>
-        <Text style={styles.texto}>X</Text>
-        <Text style={styles.texto}>X</Text>
-        <Text style={styles.texto}>X</Text>
-    </View>
-  )
-}
-
-export const Pie2=(props)=>{
-  return(
-    <View style={styles.pie}>
-        <Text style={styles.texto}>{props.opA}</Text>
-        <Text style={styles.texto}>{props.opB}</Text>
-        <Text style={styles.texto}>{props.opC}</Text>
-        <Text style={styles.texto}>{props.opD}</Text>
-    </View>
-  )
-}
-
-export const Pie3=({op1, op2, op3,op4})=>{
-  //aqui podria tener varias operaciones sin problemas 
-  return(
-    <View style={styles.pie}>
-        <Text style={styles.texto}>{op1}</Text>
-        <Text style={styles.texto}>{op2}</Text>
-        <Text style={styles.texto}>{op3}</Text>
-        <Text style={styles.texto}>{op4}</Text>
+    <View style={styles.container}>
+      <Text>Hola esto es el Home...</Text>
+      <Boton texto={'Calculadora'} accion={()=>navigation.navigate('Calculadora')}/>
+      <Boton texto={'Clima'} accion={()=>navigation.navigate('Clima')}/>
+      <Boton texto={'Contador'}/>
+      <Boton texto={'Login'}/>
+      <Boton texto={'Producto'}/>
+      <Boton texto={'Productos'}/>
     </View>
   )
 }
